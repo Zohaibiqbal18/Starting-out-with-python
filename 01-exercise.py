7c59962f63a72a2bb2258b8432e7dcb50a3a1419
Zohaib Iqbal <116112001+Zohaibiqbal18@users.noreply.github.com>
2023-11-08 09:29:25 +0500

    Delete 01-exercise.py

--- 01-exercise.py
+++
@@ -1,7 +0,0 @@
-#Personal information
-
-Name = str(input("Enter your name: "))
-Address = str(input("Enter your address: "))
-Telephone_no = int(input("Enter telephone no: "))
-
-print('My name is {} my address is {} and my telephone no is {}'.format(Name,Address,Telephone_no))

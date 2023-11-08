944c06296194e0e8146a5671b44651d4e4d13f3d
Zohaib Iqbal <116112001+Zohaibiqbal18@users.noreply.github.com>
2023-11-08 09:29:59 +0500

    Delete 03-exercise.py

--- 03-exercise.py
+++
@@ -1,7 +0,0 @@
-#Land Calculation
-
-one_acre = 43560
-total_acres = int(input("Enter total acres: "))
-no_of_acres = total_acres / one_acre
-
-print("Total no of acres in {} square feet are {}" .format(total_acres,no_of_acres))
